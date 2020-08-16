1794177fdb0ad739d3e11663e6a21faba40a4428
micxarce <micharcesierra2028@gmail.com>
2020-08-15 22:03:51 -0500

    Creados todos los métodos del TDA ArbolHuffman

--- Arbol_Huffman/src/arbol_huffman/ArbolHuffman.java
+++ Arbol_Huffman/src/arbol_huffman/ArbolHuffman.java
@@ -5,32 +5,105 @@
  */
 package arbol_huffman;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 /**
  *
  * @author josue
  */
 public class ArbolHuffman {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {    
-        String txt = Util.leerTexto("./src/arbol_huffman/texto.txt");
-        System.out.println(Util.calcularFrecuencias(txt));
-        String txt2 = Util.leerTexto("./src/arbol_huffman/prueba.txt");
-        System.out.println(Util.calcularFrecuencias(txt2));
-        // sirve
+    Node root;
+    
+    private class Node {
+        String data;
+        int frecuencia;
+        Node left;
+        Node right;
         
-        String binary = "0010 0100 1001 0010 0111 0110 1101 1011 0110 1101 1000 0000 0000 0000 0101 1011 0110 " +
-                        "1101 1011 0110 0100 1001 0010 0100 1111 1111 1111 1111 1111 1111 0101 0101 0101 0101 " +
-                        "0101 01";
-        binary = binary.replaceAll(" ", "");
-        System.out.println(binary);
-        String hex = Util.binarioHexadecimal(binary);
-        System.out.println(hex);
-        String binary2 = Util.hexadecimalBinario(hex);
-        System.out.println(binary2);
-        System.out.println(binary.equals(binary2));
+        public Node(String d, int f) {
+            data = d;
+            frecuencia = f;
+        }
+    }
+    
+    public void calcularArbol(HashMap<String,Integer> mapa) {
+        PriorityQueue<Node> nodos = convertirNodos(mapa);
+        Node retorno = null;
+        while(!nodos.isEmpty()) {
+            if (nodos.size() >= 2){
+                Node a = nodos.poll();
+                Node b = nodos.poll();
+                Node nuevo = unirNodos(a, b);
+                nodos.offer(nuevo);
+            }
+            else retorno = nodos.poll();
+        }
+        root = retorno;
+    }
+    
+    private PriorityQueue<Node> convertirNodos(HashMap<String,Integer> mapa) {
+        PriorityQueue<Node> retorno = new PriorityQueue<>((Node a, Node b) -> a.frecuencia - b.frecuencia);
+        Set<String> keys = mapa.keySet();
+        Iterator<String> it = keys.iterator();
+        while(it.hasNext()) {
+            String etiqueta = it.next();
+            int frecuencia = mapa.get(etiqueta);
+            retorno.offer(new Node(etiqueta,frecuencia));
+        }
+        return retorno;
+    }
+    
+    private Node unirNodos(Node a, Node b) {
+        int sumaFrec = a.frecuencia + b.frecuencia;
+        String dataNode = sumaFrec + "";
+        Node retorno = new Node(dataNode, sumaFrec);
+        retorno.left = a;
+        retorno.right = b;
+        return retorno;
+    }
+    
+    public HashMap<String,String> calcularCodigos() {
+        return calcularCodigos(root, "");
+    }
+    
+    private HashMap<String,String> calcularCodigos(Node n, String s) {
+        HashMap<String,String> mapa = new HashMap<>();
+        if (n == null) return mapa;
+        else if (n.left == null && n.right == null) {
+            mapa.put(n.data, s);
+            return mapa;
+        }
+        else {
+            mapa.putAll(calcularCodigos(n.left, s + "0"));
+            mapa.putAll(calcularCodigos(n.right, s + "1"));
+            return mapa;
+        }
+    }
+    
+    public static String codificar(String texto, HashMap<String,String> mapa) {
+        StringBuilder retorno = new StringBuilder();
+        String[] caracteres = texto.split("");
+        for(String i : caracteres) {
+            retorno.append(mapa.get(i));
+        }
+        return retorno.toString();
+    }
+    
+    public static String decodificar(String texto, HashMap<String,String> mapa) {
+        StringBuilder retorno = new StringBuilder();
+        String tmp = "";
+        String[] codigos = texto.split("");
+        
+        for(String i : codigos) {
+            tmp = tmp + i;
+            if (mapa.containsKey(tmp)) {
+                retorno.append(mapa.get(tmp));
+            }
+        }
+        return retorno.toString();
     }
     
 }
---
+++ Arbol_Huffman/src/ec/edu/espol/main/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.main;
+
+import arbol_huffman.Util;
+
+/**
+ *
+ * @author micharce
+ */
+public class Main {
+    public static void main(String[] args) {    
+        String txt = Util.leerTexto("./src/arbol_huffman/texto.txt");
+        System.out.println(Util.calcularFrecuencias(txt));
+        String txt2 = Util.leerTexto("./src/arbol_huffman/prueba.txt");
+        System.out.println(Util.calcularFrecuencias(txt2));
+        // sirve
+        
+        String binary = "0010 0100 1001 0010 0111 0110 1101 1011 0110 1101 1000 0000 0000 0000 0101 1011 0110 " +
+                        "1101 1011 0110 0100 1001 0010 0100 1111 1111 1111 1111 1111 1111 0101 0101 0101 0101 " +
+                        "0101 01";
+        binary = binary.replaceAll(" ", "");
+        System.out.println(binary);
+        String hex = Util.binarioHexadecimal(binary);
+        System.out.println(hex);
+        String binary2 = Util.hexadecimalBinario(hex);
+        System.out.println(binary2);
+        System.out.println(binary.equals(binary2));
+    }
+}
